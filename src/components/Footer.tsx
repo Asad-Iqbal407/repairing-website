a9ecdf41127501caf6935b4d189b7aceedb3478c
Asad-Iqbal407 <123257654+Asad-Iqbal407@users.noreply.github.com>
2025-12-05 09:35:37 +0000

    Update address in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -65,7 +65,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-2">
                 <MapPin className="h-5 w-5 text-blue-400" />
-                <span className="text-gray-400">123 Repair St, City, State</span>
+                <span className="text-gray-400">Avenida Almirante Reis 102, 1150-022 Lisboa, Portugal(Avenida Almirante Reis 102, 1150-022 Lisboa, Portugal</span>
               </div>
             </div>
           </div>
